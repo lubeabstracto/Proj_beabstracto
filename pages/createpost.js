471945290ecf1337fdbe8b503bab32ecd10edd6e
Thi <thiago.andradecontato@hotmail.com>
2024-03-03 15:35:52 +0100

    mudando o import do mypastecomponent

--- pages/createpost.js
+++ pages/createpost.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-import MyPasteComponent from '../components/MyPasteComponent';
+import MyPasteComponent from '../components/MyPasteComponent.js';
 
 const SOP = () => {
   return (
