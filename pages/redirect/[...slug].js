09b53f55bad83f6f1ff6dc255efd13e7441e81bc
Thi <thiago.andradecontato@hotmail.com>
2024-03-06 00:40:58 +0100

    adicionando logs

--- pages/redirect/[...slug].js
+++ pages/redirect/[...slug].js
@@ -1,42 +1,33 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-console.log("rodando o slug");
+console.log('rodando o slug');
 
 const RedirectPage = () => {
-  const router = useRouter();
-  const { slug } = router.query;
-  const [whatsappUrl, setWhatsappUrl] = useState('');
-
   useEffect(() => {
-    if (slug) {
-      const linkName = Array.isArray(slug) ? slug.join('/') : slug;
-      fetch(`/api/get-link?link_name=${linkName}`)
-        .then((res) => {
-          if (!res.ok) throw new Error(res.statusText);
-          return res.json();
-        })
-        .then((data) => {
-          const url = `https://api.whatsapp.com/send?phone=${data.phone_number}&text=${encodeURIComponent(data.message)}`;
-          setWhatsappUrl(url);
-        })
-        .catch((error) => {
-          console.error('Redirection error:', error);
-        });
-    }
-  }, [slug]);
+    // Suponha que você já tenha o linkName de alguma forma
+    const linkName = 'aeeecarraaaaaaalhooo';
 
-  return (
-    <>
-      {whatsappUrl && (
-        <Head>
-          <meta httpEquiv="refresh" content={`0;url=${whatsappUrl}`} />
-        </Head>
-      )}
-      <p>Redirecting...</p>
-    </>
-  );
+    // Faça uma chamada para o seu endpoint da API para obter os detalhes do link
+    fetch(`/api/get-link?link_name=${linkName}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Falha ao buscar os detalhes do link');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Construa a URL do WhatsApp com os dados obtidos
+        const whatsappUrl = `https://api.whatsapp.com/send?phone=${data.phone_number}&text=${encodeURIComponent(data.message)}`;
+        // Redirecione a janela para a URL do WhatsApp
+        window.location.href = whatsappUrl;
+      })
+      .catch(error => {
+        console.error('Erro ao redirecionar:', error);
+      });
+  }, []);
+
+  // Exiba algo na tela enquanto o redirecionamento está em processo
+  return <p>Redirecionando para o WhatsApp...</p>;
 };
 
 export default RedirectPage;
