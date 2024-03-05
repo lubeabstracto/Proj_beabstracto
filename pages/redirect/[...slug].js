51f03039d51f9fa67f7784754cb3c311f1b48e6a
Thi <thiago.andradecontato@hotmail.com>
2024-03-06 00:35:01 +0100

    mudando a abordagem para o lado do cliente

--- pages/redirect/[...slug].js
+++ pages/redirect/[...slug].js
@@ -1,33 +1,40 @@
-// pages/redirect/[...slug].js
+import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const RedirectPage = () => {
   const router = useRouter();
   const { slug } = router.query;
+  const [whatsappUrl, setWhatsappUrl] = useState('');
 
   useEffect(() => {
-    if (slug && slug.length > 0) {
-      const linkName = slug.join('/');
-      // Make API call to fetch data for the linkName
+    if (slug) {
+      const linkName = Array.isArray(slug) ? slug.join('/') : slug;
       fetch(`/api/get-link?link_name=${linkName}`)
         .then((res) => {
           if (!res.ok) throw new Error(res.statusText);
           return res.json();
         })
         .then((data) => {
-          const whatsappUrl = `https://api.whatsapp.com/send?phone=${data.phone_number}&text=${encodeURIComponent(data.message)}`;
-          window.location.href = whatsappUrl;
+          const url = `https://api.whatsapp.com/send?phone=${data.phone_number}&text=${encodeURIComponent(data.message)}`;
+          setWhatsappUrl(url);
         })
         .catch((error) => {
           console.error('Redirection error:', error);
-          // Handle error redirection
         });
     }
   }, [slug]);
 
-  // You could add a loading spinner or message here
-  return null;
+  return (
+    <>
+      {whatsappUrl && (
+        <Head>
+          <meta httpEquiv="refresh" content={`0;url=${whatsappUrl}`} />
+        </Head>
+      )}
+      <p>Redirecting...</p>
+    </>
+  );
 };
 
 export default RedirectPage;
