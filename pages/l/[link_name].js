2db55c113be100cf3b2a5f733322380923c4d251
Thi <thiago.andradecontato@hotmail.com>
2024-03-05 19:50:59 +0100

    adicionando o vercel json

--- pages/l/[link_name].js
+++ pages/l/[link_name].js
@@ -6,39 +6,31 @@ const LinkRedirect = () => {
   const { link_name } = router.query;
 
   useEffect(() => {
-    // Log to verify we're getting into the useEffect block
-    console.log('Attempting to redirect for link_name:', link_name);
-
     if (link_name) {
-      // Log the URL we're about to fetch
-      console.log('Fetching link details for:', link_name);
-
+      // This fetch call should be to the backend URL where your database is hosted.
+      // If you're deploying to Vercel and the backend is also there, the relative path '/api/get-link' is fine.
+      // If your backend is hosted elsewhere, you'll need to provide the full URL to that service.
       fetch(`/api/get-link?link_name=${link_name}`)
         .then((res) => {
-          if (res.ok) return res.json();
-          throw new Error('Failed to fetch link details');
+          if (!res.ok) throw new Error(res.statusText);
+          return res.json();
         })
         .then((data) => {
-          // Log the fetched data
-          console.log('Fetched data:', data);
-
-          const whatsappUrl = `https://wa.me/${data.phone_number}?text=${encodeURIComponent(data.message)}`;
-          // Log the WhatsApp URL we're about to redirect to
-          console.log('Redirecting to WhatsApp URL:', whatsappUrl);
-
+          // Assuming `data` has the phone_number and message
+          // If your environment doesn't support window.location redirection, consider other methods like window.location.assign
+          const whatsappUrl = `https://api.whatsapp.com/send?phone=${data.phone_number}&text=${encodeURIComponent(data.message)}`;
           window.location.href = whatsappUrl;
         })
         .catch((error) => {
-          // Log any errors
-          console.error('Error fetching link details:', error);
+          console.error('Redirection error:', error);
+          // Here you should handle what happens if the link_name doesn't exist or there's another error.
+          // This could be showing an error message to the user.
         });
-    } else {
-      // Log if link_name is not present
-      console.log('Link name is not defined in the query');
     }
   }, [link_name]);
 
-  return <p>Redirecting...</p>;
+  // You could add some user-friendly message or loading spinner here.
+  return <p>Redirecting you to WhatsApp...</p>;
 };
 
 export default LinkRedirect;
