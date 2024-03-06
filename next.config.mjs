92960a79e181d0659cdf3a55189d89df30952618
Thi <thiago.andradecontato@hotmail.com>
2024-03-06 12:34:37 +0100

    removendo os rewrites do next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 // next.config.mjs
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/l/:path*',
-        destination: '/gerador-link-whats-app/:path*', // Adjust this as needed
-      },
-    ];
-  },
 };
 
 export default nextConfig;
--- pages/l/[link_name].js
+++ pages/l/[link_name].js
@@ -1,9 +1,7 @@
 // pages/l/[link_name].js
 import { Pool } from 'pg';
 
-export default function LinkRedirect({ whatsappLink }) {
-  // Client-side redirection isn't necessary here because we'll handle redirection server-side
-
+export default function LinkRedirect() {
   // Render nothing or a loading indicator since we should redirect before this renders
   return null;
 }
@@ -20,16 +18,16 @@ export async function getServerSideProps(context) {
     // Perform your database query here using server-side code
     const { rows } = await pool.query('SELECT whatsapp_link FROM links WHERE link_name = $1', [link_name]);
     
-    if (rows.length > 0) {
+    if (rows.length > 0 && rows[0].whatsapp_link) {
       // Redirect to the WhatsApp link
       return {
         redirect: {
           destination: rows[0].whatsapp_link,
-          permanent: false, // This is usually a temporary redirect as the link might change
+          permanent: false,
         },
       };
     } else {
-      // If no record is found, return a 404
+      // If no record is found or whatsapp_link is null, return a 404
       return {
         notFound: true,
       };
