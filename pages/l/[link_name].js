e999ef7e9dbc32d0ff0d8c59fc6275af5b8ceb14
Thi <thiago.andradecontato@hotmail.com>
2024-03-06 11:41:01 +0100

    atualizando o link_name

--- pages/l/[link_name].js
+++ pages/l/[link_name].js
@@ -1,46 +1,29 @@
-import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-console.log("rodando o link name");
+const RedirectPage = () => {
+    const router = useRouter();
+    const { slug } = router.query; // Deve capturar o "dkdjjdjddjdj" da URL
+  
+    useEffect(() => {
+      // Certifique-se de que este log só ocorra quando o slug estiver definido
+      if (slug) {
+        console.log(`O slug é: ${slug}`); // Deve logar "O slug é: dkdjjdjddjdj"
+      }
+    }, [slug]);
 
-const LinkRedirect = () => {
-  const router = useRouter();
-  const { link_name } = router.query;
-
-  useEffect(() => {
-    // Log to verify we're getting into the useEffect block
-    console.log('Attempting to redirect for link_name:', link_name);
-
-    if (link_name) {
-      // Log the URL we're about to fetch
-      console.log('Fetching link details for:', link_name);
-
-      fetch(`/api/get-link?link_name=${link_name}`)
-        .then((res) => {
-          if (res.ok) return res.json();
-          throw new Error('Failed to fetch link details');
-        })
-        .then((data) => {
-          // Log the fetched data
-          console.log('Fetched data:', data);
-
-          const whatsappUrl = `https://wa.me/${data.phone_number}?text=${encodeURIComponent(data.message)}`;
-          // Log the WhatsApp URL we're about to redirect to
-          console.log('Redirecting to WhatsApp URL:', whatsappUrl);
-
-          window.location.href = whatsappUrl;
-        })
-        .catch((error) => {
-          // Log any errors
-          console.error('Error fetching link details:', error);
-        });
-    } else {
-      // Log if link_name is not present
-      console.log('Link name is not defined in the query');
-    }
-  }, [link_name]);
-
-  return <p>Redirecting...</p>;
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>Click the button below to go to the Create Post page.</p>
+      <Link href="/createpost">
+        <a>
+          <button>Create Post</button>
+        </a>
+      </Link>
+    </div>
+  );
 };
 
-export default LinkRedirect;
+export default RedirectPage;
